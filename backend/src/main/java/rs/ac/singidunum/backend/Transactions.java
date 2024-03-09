df800d639b8e7eca549abcd9ff9e568eabe970b4
Dimitrije Rankov <dimitrije.rankov@outlook.com>
2024-03-09 20:34:51 +0100

    creating entities and configuring propreties

---
+++ backend/src/main/java/rs/ac/singidunum/backend/Transactions.java
@@ -0,0 +1,43 @@
+package rs.ac.singidunum.backend;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TRANSACTIONS")
+public class Transactions {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int transactionId;
+
+    @ManyToOne
+    @JoinColumn(name = "buyer", referencedColumnName = "user_id")
+    private Users buyer;
+
+    @ManyToOne
+    @JoinColumn(name = "product", referencedColumnName = "bike_id")
+    private Bikes product;
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public Users getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(Users buyer) {
+        this.buyer = buyer;
+    }
+
+    public Bikes getProduct() {
+        return product;
+    }
+
+    public void setProduct(Bikes product) {
+        this.product = product;
+    }
+}
---
+++ backend/src/main/java/rs/ac/singidunum/backend/Users.java
@@ -0,0 +1,55 @@
+package rs.ac.singidunum.backend;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "USERS")
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+    private String email;
+    private String passwordHash;
+    private double balance;
+    private String creationDate;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        this.creationDate = creationDate;
+    }
+}
