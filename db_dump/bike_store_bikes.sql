5f768c97496397469da0f5da6d078d5ea2382ac1
44filip <100999946+44filip@users.noreply.github.com>
2024-03-10 01:01:55 +0100

    removing redundant .sql files

--- db_dump/bike_store_bikes.sql
+++ db_dump/bike_store_bikes.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `bike_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bike_store`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bike_store
@@ -37,7 +39,7 @@ CREATE TABLE `bikes` (
 
 LOCK TABLES `bikes` WRITE;
 /*!40000 ALTER TABLE `bikes` DISABLE KEYS */;
-INSERT INTO `bikes` VALUES (1,'Cycle',219.99,'/images/img-2.png'),(2,'Stylish',279.99,'/images/img-3.png'),(3,'Modern',249.99,'/images/img-4.png'),(4,'Classic',239.99,'/images/img-5.png'),(5,'Supreme',259.99,'/images/img-6.png'),(6,'Optimal',299.99,'/images/img-1.png');
+INSERT INTO `bikes` VALUES (1,'Cycle',219.99,'/images/img-1.png'),(2,'Stylish',279.99,'/images/img-2.png'),(3,'Modern',249.99,'/images/img-3.png'),(4,'Classic',239.99,'/images/img-4.png'),(5,'Supreme',259.99,'/images/img-5.png'),(6,'Optimal',299.99,'/images/img-6.png');
 /*!40000 ALTER TABLE `bikes` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -50,4 +52,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-09 23:19:35
+-- Dump completed on 2024-03-10  1:01:42
--- db_dump/bike_store_routines.sql
+++ db_dump/bike_store_routines.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `bike_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bike_store`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bike_store
@@ -32,4 +34,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-03-09 23:19:35
+-- Dump completed on 2024-03-10  1:01:43
