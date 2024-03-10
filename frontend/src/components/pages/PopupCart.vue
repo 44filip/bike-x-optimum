ac33335f082868f9866777eb8f94ae45fe621d2a
44filip <100999946+44filip@users.noreply.github.com>
2024-03-10 12:08:33 +0100

    opacity and popup changes

--- frontend/src/components/pages/PopupCart.vue
+++ frontend/src/components/pages/PopupCart.vue
@@ -17,7 +17,7 @@ export default {
             this.show = true;
             setTimeout(() => {
                 this.show = false;
-            }, 2000); // Hide after 2 seconds
+            }, 1000); // Hide after 1 second
         },
     },
 };
@@ -30,7 +30,7 @@ export default {
     left: 50%;
     transform: translate(-50%, -50%);
     background-color: #f9f9f9;
-    padding: 20px;
+    padding: 40px;
     border: 1px solid #ccc;
     z-index: 1000;
 }
