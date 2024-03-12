8def992552f5eb7e1139df4114373becb94c9134
44filip <100999946+44filip@users.noreply.github.com>
2024-03-12 02:36:51 +0100

    capitalization fix for login after logout

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -14,7 +14,7 @@
                <li v-for="item, index in navItems" :key="index" class="nav-item">
                   <a class="nav-link active"><router-link :to="item.path">{{ item.text }}</router-link></a>
                </li>
-               <li  v-role="['unauthorized']" class="nav-item"><a class="nav-link active"><router-link to="/login">login</router-link></a></li>
+               <li  v-role="['unauthorized']" class="nav-item"><a class="nav-link active"><router-link to="/login">Login</router-link></a></li>
                <li  v-role="['admin', 'user']" @click="logout" class="nav-item"><a href="#" class="nav-link active">Logout</a></li>
             </ul>
             <div class="login_menu">
