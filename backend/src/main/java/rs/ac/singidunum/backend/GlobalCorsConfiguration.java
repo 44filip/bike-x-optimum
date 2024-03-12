101dbe08899502302fc94429e4059377ae0935b3
44filip <100999946+44filip@users.noreply.github.com>
2024-03-12 04:14:33 +0100

    supress warnings for null

--- backend/src/main/java/rs/ac/singidunum/backend/GlobalCorsConfiguration.java
+++ backend/src/main/java/rs/ac/singidunum/backend/GlobalCorsConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class GlobalCorsConfiguration implements WebMvcConfigurer {
 
+    @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
