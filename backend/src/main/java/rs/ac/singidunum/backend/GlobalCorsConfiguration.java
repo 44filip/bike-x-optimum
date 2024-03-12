219456d9fbe949ded8c65d617ca21a550a6f920f
44filip <100999946+44filip@users.noreply.github.com>
2024-03-12 04:23:35 +0100

    GlobalCorsConfig fix

---
+++ backend/src/main/java/rs/ac/singidunum/backend/GlobalCorsConfiguration.java
@@ -0,0 +1,20 @@
+package rs.ac.singidunum.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class GlobalCorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080", "http://localhost:8081")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
