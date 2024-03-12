cf1da2fe977bc9d4deeb0fca086bf48e9868d2d9
44filip <100999946+44filip@users.noreply.github.com>
2024-03-12 04:12:58 +0100

    cross origin fix

---
+++ backend/src/main/java/rs/ac/singidunum/backend/GlobalCorsConfiguration.java
@@ -0,0 +1,18 @@
+package rs.ac.singidunum.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
