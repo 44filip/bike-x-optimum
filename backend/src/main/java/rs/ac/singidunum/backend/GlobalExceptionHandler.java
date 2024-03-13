beb0ffc92d3342ddb3662d32df9364a207722e3d
44filip <100999946+44filip@users.noreply.github.com>
2024-03-13 03:59:00 +0100

    Create GlobalExceptionHandler.java

---
+++ backend/src/main/java/rs/ac/singidunum/backend/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package rs.ac.singidunum.backend;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest().body("Invalid request payload.");
+    }
+}
