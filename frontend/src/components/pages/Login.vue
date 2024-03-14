a031378d59943f6c201e73462508919e677cf5fd
44filip <100999946+44filip@users.noreply.github.com>
2024-03-14 20:56:59 +0100

    Login and register style changes

--- frontend/src/components/pages/Login.vue
+++ frontend/src/components/pages/Login.vue
@@ -3,14 +3,14 @@
         <div class="form-grou">
             <h1>Login</h1>
             <form class='login-form' @submit.prevent="performLogin">
-                <div class="form-group">
+                <div class="forma">
                     <input v-model="username" id="email" class='lf--input form-control' placeholder='E-mail' type='text'>
                 </div>
-                <div class="form-group">
+                <div class="forma">
                     <input v-model="password" id="password" class='lf--input form-control' placeholder='Password'
                         type='password'>
                 </div>
-                <div class="form-group">
+                <div class="forma">
                     <input class='lf--submit form-control' type='submit' value='LOGIN'>
                 </div>
             </form>
--- frontend/src/components/pages/Register.vue
+++ frontend/src/components/pages/Register.vue
@@ -1,20 +1,22 @@
 <template>
-    <div class="form-grou">
-        <h1>Register</h1>
-        <form class='login-form' @submit.prevent="performRegister">
-            <div class="forma">
-                <input v-model="email" id="email" class='lf--input form-control' placeholder='E-mail' type='text'>
-            </div>
-            <div class="forma">
-                <input v-model="password" id="password" class='lf--input form-control' placeholder='Password'
-                    type='password'>
-            </div>
-            <div class="forma">
-                <input class='lf--submit form-control' type='submit' value='REGISTER'>
-            </div>
-        </form>
-        <a class='lf--forgot'><router-link to="/login">Already have an account?<br>Login HERE</router-link></a>
-        <popup-error ref="popup"></popup-error>
+    <div class="page-size">
+        <div class="form-grou">
+            <h1>Register</h1>
+            <form class='login-form' @submit.prevent="performRegister">
+                <div class="forma">
+                    <input v-model="email" id="email" class='lf--input form-control' placeholder='E-mail' type='text'>
+                </div>
+                <div class="forma">
+                    <input v-model="password" id="password" class='lf--input form-control' placeholder='Password'
+                        type='password'>
+                </div>
+                <div class="forma">
+                    <input class='lf--submit form-control' type='submit' value='REGISTER'>
+                </div>
+            </form>
+            <a class='lf--forgot'><router-link to="/login">Already have an account?<br>Login HERE</router-link></a>
+            <popup-error ref="popup"></popup-error>
+        </div>
     </div>
 </template>
 <script>
