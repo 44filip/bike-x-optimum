5b642ab4bb8538f3c115e62d63041e6e302e960f
44filip <100999946+44filip@users.noreply.github.com>
2025-06-09 15:05:11 +0200

    Added included schema and consistent pricing to dump

--- db_dump/bike_store_routines.sql
+++ db_dump/bike_store_routines.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `bike_store` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bike_store`;
 -- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bike_store
@@ -32,4 +34,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-09  6:47:29
+-- Dump completed on 2025-06-09 15:04:10
