996ff0d7f567d14ffefb991c3e6c3d06ba958488
44filip <100999946+44filip@users.noreply.github.com>
2025-06-12 16:18:53 +0200

    Removed excess anchor on router for login

--- frontend/src/components/pages/RegisterdLogin.vue
+++ frontend/src/components/pages/RegisterdLogin.vue
@@ -2,7 +2,7 @@
   <div v-role="['unauthorized']" id="emptyCart" bis_skin_checked="1">
     <h2>You have registered, please log in.</h2>
     <div class="remove_btn col-md-6">
-      <a><router-link to="/login">Click here!</router-link></a>
+      <router-link to="/login">Click here!</router-link>
     </div>
   </div>
 </template>
--- frontend/src/components/pages/Shop.vue
+++ frontend/src/components/pages/Shop.vue
@@ -87,7 +87,7 @@
       <div v-role="['unauthorized']" id="emptyCart" bis_skin_checked="1">
         <h2>Please log in.</h2>
         <div class="remove_btn col-md-6">
-          <a><router-link to="/login">Click here!</router-link></a>
+          <router-link to="/login">Click here!</router-link>
         </div>
       </div>
     </div>
